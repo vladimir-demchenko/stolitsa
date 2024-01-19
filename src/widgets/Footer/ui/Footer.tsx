7a3fe1fd175f06efc65c6e91322459ca07bfc1c6
Vladimir <jaln.102406@gmail.com>
2024-01-19 12:08:21 +0300

    fix

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,5 +1,7 @@
 import LogoFooter from 'shared/assets/icons/logo_footer.svg';
 import LogoFooterMobile from 'shared/assets/icons/logo_footer_mobile.svg';
+import TelegramIcon from 'shared/assets/icons/telegram_icon.svg';
+import EmailIcon from 'shared/assets/icons/email_icon.svg';
 import cls from './Footer.module.scss';
 import { isMobile } from 'react-device-detect';
 
@@ -20,8 +22,8 @@ export const Footer = () => {
         <div className={cls.footerSocial}>
           <h4 className={cls.footerSubtitle}>Контакты</h4>
           <ul className={cls.footerList}>
-            <li className={cls.footerItem}><a className={cls.footerLink} href="">@STOlitsa_Leto</a></li>
-            <li className={cls.footerItem}><a className={cls.footerLink} href="mailto:stolitsaleto@anogn.ru">stolitsaleto@anogn.ru</a></li>
+            <li className={cls.footerItem}><TelegramIcon className={cls.footerIcon} /> <a className={cls.footerLink} href="https://t.me/STOlitsa_Leto">@STOlitsa_Leto</a></li>
+            <li className={cls.footerItem}><EmailIcon className={cls.footerIcon} /> <a className={cls.footerLink} href="mailto:stolitsaleto@anogn.ru">stolitsaleto@anogn.ru</a></li>
           </ul>
         </div>
       </div>
--- src/widgets/Partners/ui/Partners.tsx
+++ src/widgets/Partners/ui/Partners.tsx
@@ -17,19 +17,29 @@ export const Partners = () => {
       <h2 className={cls.title}>при поддержке</h2>
       <div className={cls.partnersList}>
         <div className={cls.slide}>
-          <Kosimp />
+          <a className={cls.partnersLink} href="https://www.mos.ru/kos/" target='_blank'>
+            <Kosimp />
+          </a>
         </div>
-        <div className={cls.slide}><StolitsaBlack /></div>
+        <div className={cls.slide}><a className={cls.partnersLink} href="https://vk.com/stolitsa_msk" target='_blank'>
+          <StolitsaBlack />
+        </a></div>
         <div className={cls.slide}>
-          <MosMold />
+          <a className={cls.partnersLink} href="https://mosmolodezh.ru/" target='_blank'>
+            <MosMold />
+          </a>
         </div>
         <div className={cls.slide}>
-          <Koworking />
+          <a className={cls.partnersLink} href="https://dushevnayamoskva.ru/" target='_blank'>
+            <Koworking />
+          </a>
         </div>
         <div className={cls.slide}>
-          <Mosvol />
+          <a className={cls.partnersLink} href="https://mosvolonter.ru/" target='_blank'>
+            <Mosvol />
+          </a>
         </div>
-        <div className={cls.slide}><img className={cls.logoImg} alt="" src={GN_logo} /></div>
+        <div className={cls.slide}><a className={cls.partnersLink} href="https://vk.com/gorodneravnodushnykh" target='_blank'><img className={cls.logoImg} alt="" src={GN_logo} /></a></div>
       </div>
       {/* <svg className={classNames('curves', {}, [cls.purpleCurve])} xmlns="http://www.w3.org/2000/svg" width="697" height="917" viewBox="0 0 697 917" fill="none">
         <path d="M689.086 8.02246C544.788 71.0435 275.447 212.107 352.464 272.191C448.736 347.297 636.604 221.755 436.248 197.243C235.893 172.731 -192.555 371.967 115.721 564.155C362.341 717.906 361.286 858.372 329.931 909.386" stroke="#701487" stroke-width="15" stroke-linecap="round" />
